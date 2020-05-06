a22ad917ea843b2effe9efacfa30b546775ebbea
fg14 <filip.geppert@gmail.com>
2020-05-06 09:55:20 +0200

    Add static

--- labeller/static/js/drawing.js
+++ labeller/static/js/drawing.js
@@ -1,4 +1,6 @@
 let cv = document.getElementById('cv');
+let cvOffsetX = cv.getBoundingClientRect().x;
+let cvOffsetY = cv.getBoundingClientRect().y;
 let canvas = document.getElementById('imageCanvas');
 let ctx = canvas.getContext('2d');
 let rect = {}, drag = false, selections = [];
@@ -9,9 +11,41 @@ let annotations = document.getElementById('annotations');
 let imageId = null;
 let id_count = 0;
 let current_filename = "";
+let messageBox = document.getElementById("message");
+let messageLarge = document.getElementById("messageLarge");
+let messageSmall = document.getElementById("messageSmall");
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function getDeleteButton(selectionId) {
+    return `<a id="delete${selectionId}" onclick="removeSelection(${selectionId})">`
+        + 'Delete</a>';
+}
+
+function getSelectionTemplate(category, id) {
+    return '<li class="list-group-item d-flex justify-content-between align-items-center m-1">'
+        + `${category}`
+        +  `<span class="badge badge-danger badge-pill">${getDeleteButton(id)}</span>`
+        +'</li>';
+}
+
 
 class Selection {
-    constructor(x,y,w,h,category) {
+    constructor(x, y, w, h, category) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -21,38 +55,46 @@ class Selection {
     }
 }
 
-function sendDataAJAX (url, data) {
-      $.ajax({
+function sendDataAJAX(url, data) {
+    $.ajax({
         url: url,
         data: data,
         dataType: 'json',
         success: function (data) {
-            console.log("Data saved.")
+            messageLarge.innerText = "Annotations successfully saved!"
+            messageSmall.innerText = current_filename;
+            messageBox.style.display ="inline";
+            setTimeout(() => messageBox.style.display = "none", 3000)
         }
-      });
+    });
 }
 
 function sendImageAJAX(img) {
     let formData = new FormData();
     // let file = $('#cv')[0].files[0];
     formData.append('image', img);
-    // TODO: add csrf token
-    // https://stackoverflow.com/questions/6506897/csrf-token-missing-or-incorrect-while-post-parameter-via-ajax-in-django
+
     $.ajax({
         url: 'saveImage',
         type: 'post',
         data: formData,
+        headers: {
+            'X-CSRFToken': getCookie('csrftoken'),
+        },
         contentType: false,
         processData: false,
         success: function (data) {
             imageId = data.imageId;
+            console.log("Image was saved.")
         },
-        error: function () {
+        error: function(xhr, status, error) {
             // TODO: add modal info that data was not saved
+            console.log(xhr.responseText);
             console.log("There was a problem with saving image.")
         }
     });
 }
+
 
 function init() {
     canvas.addEventListener('mousedown', mouseDown, false);
@@ -80,34 +122,36 @@ function init() {
 
 
 function mouseDown(e) {
-  rect.startX = e.pageX;
-  rect.startY = e.pageY;
-  drag = true;
+    rect.startX = e.pageX - cvOffsetX;
+    rect.startY = e.pageY - cvOffsetY;
+    drag = true;
 }
 
 function mouseUp() {
-  drag = false;
-  showInput(rect);
+    drag = false;
+    showInput(rect);
 }
 
 function mouseMove(e) {
-  if (drag) {
-    hideInput();
-    rect.w = (e.pageX) - rect.startX;
-    rect.h = (e.pageY) - rect.startY ;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    draw();
-  }
+    if (drag) {
+        hideInput();
+        rect.w = (e.pageX - cvOffsetX) - rect.startX;
+        rect.h = (e.pageY - cvOffsetY) - rect.startY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        draw();
+    }
 }
 
 function draw() {
-  ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+    console.log(rect);
+    ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
 }
+
 
 function showInput(rect) {
     let input = document.getElementById('categoryLabel');
     input.style.display = "inline";
-    input.style.left = `${rect.w - 100}px`;
+    input.style.left = `${cvOffsetX}px`;
     input.style.top = `${rect.startY + 10}px`;
     let textCategory = document.getElementById('textCategory');
     textCategory.value = "";
@@ -120,17 +164,13 @@ function hideInput() {
 
 function displaySelections(selections) {
     let content = "";
-    for (let i=0; i < selections.length; i++) {
-        content += '<div class="alert alert-primary row" role="alert">'
-            + `<div class="col-4 text-black-50 font-weight-bold">${selections[i].category}</div>`
-            // + getCategoriesForm(selections[i].id)
-            + getDeleteButton(selections[i].id)
-            + '</div>';
+    for (let i = 0; i < selections.length; i++) {
+        content += getSelectionTemplate(selections[i].category, selections[i].id)
     }
     annotations.innerHTML = content;
 }
 
-function saveSelection () {
+function saveSelection() {
     let submitValue = document.getElementById('textCategory');
     rect.category = submitValue.value.slice();
     let s = new Selection(rect.startX, rect.startY, rect.w, rect.h, rect.category);
@@ -144,18 +184,11 @@ function removeSelection(selectionId) {
     displaySelections(selections);
 }
 
-function getDeleteButton(selectionId) {
-    let html = '<div class="col-2">'
-        + `<a class="btn border-danger text-dark" id="delete${selectionId}" onclick="removeSelection(${selectionId})">`
-        + 'Delete</a>'
-        + '</div>'
-    return html;
-}
 
 function openFile(event) {
     let input = event.target;
     let reader = new FileReader();
-    reader.onload = function(){
+    reader.onload = function () {
         let dataURL = reader.result;
         let output = document.getElementById('cv');
         output.src = dataURL;
